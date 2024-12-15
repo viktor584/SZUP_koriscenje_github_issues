6b75b8b22bb3dc10302b7085d8478bcadd603f7b
viktor584 <128515124+viktor584@users.noreply.github.com>
2024-12-15 23:29:16 +0100

    Update Grešku u validaciji korisnika.js

--- Grešku u validaciji korisnika.js
+++ Grešku u validaciji korisnika.js
@@ -1,12 +1,1 @@
-function isValidEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
-
-// Primer korišćenja:
-const email = "test@example.com";
-if (isValidEmail(email)) {
-    console.log("Email je ispravan.");
-} else {
-    console.log("Email je neispravan.");
-}
+nano validation.js
